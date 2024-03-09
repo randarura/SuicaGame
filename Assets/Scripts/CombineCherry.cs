0cab0189a3cb8c0bd1d0be64952ebefaab4617df
randarura <takufuku77@icloud.com>
2024-03-09 21:00:58 +0900

    Add ignore

---
+++ Assets/Scripts/CombineCherry.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CombineCherry : FruitCollisionHandler
+{
+    // 衝突した際の処理
+    protected override void OnCollisionEnter2D(Collision2D other)
+    {
+        base.OnCollisionEnter2D(other); // 親クラスの処理を実行
+    }
+}
+
